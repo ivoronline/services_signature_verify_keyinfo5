f9adf04f471488356f02460af92a3d33ffb7fa45
igoreta <ivoronline@gmail.com>
2021-06-13 22:31:14 +0200

    Commit

--- src/main/java/ValidateSignature.java
+++ src/main/java/ValidateSignature.java
@@ -3,13 +3,15 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import javax.xml.crypto.MarshalException;
 import javax.xml.crypto.dsig.XMLSignature;
+import javax.xml.crypto.dsig.XMLSignatureException;
 import javax.xml.crypto.dsig.XMLSignatureFactory;
 import javax.xml.crypto.dsig.dom.DOMValidateContext;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.FileInputStream;
 
-public class VerifySignature {
+public class ValidateSignature {
 
   static String fileXMLInput = "src/main/resources/PersonSigned.xml";
 
@@ -19,21 +21,34 @@ public class VerifySignature {
   public static void main(String[] args) throws Exception {
 
     //READ XML FROM FILE
-    Document            document = readXMLFromFile(fileXMLInput);
+    Document document = readXMLFromFile(fileXMLInput);
+
+    //VALIDATE XML
+    boolean valid = validateSignature(document, "Person");
+
+    //DISPLAY RESULT
+    System.out.println(valid);
+
+  }
+
+  //================================================================================
+  // VALIDATE SIGNATURE
+  //================================================================================
+  private static boolean validateSignature(Document document, String elementName) throws Exception  {
 
     //GET SIGNATURE NODE
     Node                signatureNode = document.getElementsByTagNameNS(XMLSignature.XMLNS, "Signature").item(0);
 
     //VALIDATE SIGNATURE
-    Element             element    = (Element) document.getElementsByTagName("Person").item(0);           //FIX
+    Element             element    = (Element) document.getElementsByTagName(elementName).item(0);     //FIX
     DOMValidateContext  valContext = new DOMValidateContext(new KeyValueKeySelector(), signatureNode);
-                        valContext.setIdAttributeNS((Element) signatureNode.getParentNode(), null, "Id"); //FIX
+    valContext.setIdAttributeNS((Element) signatureNode.getParentNode(), null, "Id");                  //FIX
     XMLSignatureFactory factory    = XMLSignatureFactory.getInstance("DOM");
     XMLSignature        signature  = factory.unmarshalXMLSignature(valContext);
     boolean             valid      = signature.validate(valContext);
 
-    //DISPLAY RESULT
-    System.out.println(valid);
+    //RETURN RESULT
+    return valid;
 
   }
 
